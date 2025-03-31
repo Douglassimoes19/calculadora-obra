06ebe80e2f5fa18aee0a6a530b3136b9c2d2a5c7
douglassimoes19 <douglass.vieira2014@gmail.com>
2025-03-31 16:22:46 -0300

    versão estruturada

---
+++ script.js
@@ -0,0 +1,69 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getFirestore, collection, addDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAjjZ4conWjZQTRpNywR7REAXbr2N9KGXQ",
+    authDomain: "calculadora-obra-8ba05.firebaseapp.com",
+    projectId: "calculadora-obra-8ba05",
+    storageBucket: "calculadora-obra-8ba05.firebasestorage.app",
+    messagingSenderId: "757167180935",
+    appId: "1:757167180935:web:ef0b1ec069267019f57c22",
+    measurementId: "G-GK035YE8HR"
+};
+    
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+document.getElementById("form").addEventListener("submit",function(event){
+    event.preventDefault();
+    const area = Number(document.getElementById("area").value);
+    const custoMaterial = Number(document.getElementById("custo").value);
+    const mao_obra = Number(document.getElementById("mao_obra").value);
+    if (isNaN(area) || isNaN(custoMaterial) || isNaN(mao_obra) || area <= 0) {
+        alert("Por favor, insira valores válidos.");
+        return;
+    }
+    calcularCusto(area, custoMaterial,mao_obra);
+})
+    
+    
+async function calcularCusto(area, custoMaterial, mao_obra){
+    try{
+        let custo_total = Number((mao_obra + custoMaterial) / area);
+        await addDoc(collection(db,"custos"), {
+            area_total:area,
+            custo_mao_obra: mao_obra,
+            custo_material:custoMaterial,
+            custo_m2:custo_total
+        })
+        alert("Executando cálculo...");
+        document.getElementById("form").reset();
+        showConta();
+    }catch(error){
+        console.error('ERRO AO FAZER O CALCULO',error);
+        alert("erro ao calcular");
+    }
+}
+
+function showConta() {
+    const listaCalculos = document.getElementById("listaCalculos");
+    listaCalculos.innerHTML = "<li>...</li>";
+
+    onSnapshot(collection(db, "custos"), (snapshot) => {
+        listaCalculos.innerHTML = "";
+        snapshot.forEach(doc => {
+            const custos = doc.data();
+            const material = parseFloat(custos.custo_material);
+            const mao_obra = parseFloat(custos.custo_mao_obra);
+            const total = parseFloat(custos.custo_m2);
+            listaCalculos.innerHTML += `
+                <li>{
+                    <span> Área: <b>${custos.area_total}m²</b> </span> ;
+                    <span> Materiais: <b>R$${material.toFixed(2)}</b></span>; 
+                    <span> Mão de Obra: <b>R$${mao_obra.toFixed(2)}</b></span>; 
+                    <span> <strong>O Custo total da obra por m²: R$${total.toFixed(2)}</strong></span>}
+                </li>
+            `;
+        });
+    });
+}
